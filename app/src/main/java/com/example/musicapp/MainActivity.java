d76ceaabc446d2e691c795a4c291f300f3b2fcd7
yhz1651 <572274170@qq.com>
2022-06-21 19:40:55 +0800

    fragment3

--- app/src/main/java/com/example/musicapp/MainActivity.java
+++ app/src/main/java/com/example/musicapp/MainActivity.java
@@ -1,15 +1,22 @@
 package com.example.musicapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 //App主界面
 public class MainActivity extends AppCompatActivity {
 
-    //lc lc lc yyds
     private Button bnt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +24,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 //        Intent intent = new Intent(this, PlayMusicActivity.class);
 //        startActivity(intent);
+
+        // 步骤1：获取FragmentManager
+        FragmentManager fragmentManager = getFragmentManager();
+        // 步骤2：获取FragmentTransaction
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        NavController navController = Navigation.findNavController(this,R.id.fragment);
+        AppBarConfiguration configuration = new AppBarConfiguration.Builder(navController.getGraph()).build();
+        NavigationUI.setupWithNavController(bottomNavigationView,navController);
 
     }
 }
