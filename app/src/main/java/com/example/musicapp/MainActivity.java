9f248f2609a12abe4eca2023ce9ea68ec04c71b7
yhz1651 <572274170@qq.com>
2022-06-21 12:01:42 +0800

    activity2

--- app/src/main/java/com/example/musicapp/MainActivity.java
+++ app/src/main/java/com/example/musicapp/MainActivity.java
@@ -15,8 +15,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent intent = new Intent(this, PlayMusicActivity.class);
-        startActivity(intent);
+//        Intent intent = new Intent(this, PlayMusicActivity.class);
+//        startActivity(intent);
 
     }
 }
