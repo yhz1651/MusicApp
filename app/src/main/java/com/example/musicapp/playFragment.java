b07390fee0674c689416f8e50be2a0a00ddc4c3d
yhz1651 <572274170@qq.com>
2022-06-21 17:33:25 +0800

    fragment

---
+++ app/src/main/java/com/example/musicapp/playFragment.java
@@ -0,0 +1,36 @@
+package com.example.musicapp;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class playFragment extends Fragment {
+
+    private PlayViewModel mViewModel;
+
+    public static playFragment newInstance() {
+        return new playFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.play_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(PlayViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
