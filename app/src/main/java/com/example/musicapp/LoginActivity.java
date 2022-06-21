4cd1a7118216150f1805c7b0f3e8148706de8747
yhz1651 <572274170@qq.com>
2022-06-21 11:54:27 +0800

    activity

--- app/src/main/java/com/example/musicapp/LoginActivity.java
+++ app/src/main/java/com/example/musicapp/LoginActivity.java
@@ -4,19 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
 
-//App主界面
-public class MainActivity extends AppCompatActivity {
+//登录界面，登录成功进入主界面
+public class LoginActivity extends AppCompatActivity {
 
-    //lc lc lc yyds
-    private Button bnt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Intent intent = new Intent(this, PlayMusicActivity.class);
+        setContentView(R.layout.activity_login);
+        Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
-
     }
 }
--- app/src/main/java/com/example/musicapp/RegisterActivity.java
+++ app/src/main/java/com/example/musicapp/RegisterActivity.java
@@ -4,19 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
 
-//App主界面
-public class MainActivity extends AppCompatActivity {
+//注册界面，注册成功返回登录界面
+public class RegisterActivity extends AppCompatActivity {
 
-    //lc lc lc yyds
-    private Button bnt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Intent intent = new Intent(this, PlayMusicActivity.class);
+        setContentView(R.layout.activity_register);
+        Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
-
     }
 }
