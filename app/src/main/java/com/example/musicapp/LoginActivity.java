2c6259f8397d2341bd6721672387239f774f8cf7
Jasonqiang <2330409631@qq.com>
2022-06-22 11:29:32 +0800

    jason

--- app/src/main/java/com/example/musicapp/LoginActivity.java
+++ app/src/main/java/com/example/musicapp/LoginActivity.java
@@ -1,18 +1,70 @@
 package com.example.musicapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
-import android.os.Bundle;
+//import androidx.appcompat.app.AppCompatActivity;
+//
+//import android.content.Intent;
+//import android.os.Bundle;
 
 //登录界面，登录成功进入主界面
+import android.content.Intent;
+import androidx.appcompat.app.AppCompatActivity;
+//import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.musicapp.service.UserService;
+
 public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
+        setContentView(R.layout.activity_main);//即activity_login.xml
+        findViews();
+    }
+    private EditText username;
+    private EditText password;
+    private Button login;
+    private Button register;
+
+    private void findViews() {
+        username=(EditText) findViewById(R.id.username);
+        password=(EditText) findViewById(R.id.password);
+        login=(Button) findViewById(R.id.login);
+        register=(Button) findViewById(R.id.register);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name=username.getText().toString();
+                System.out.println(name);
+                String pass=password.getText().toString();
+                System.out.println(pass);
+
+                Log.i("TAG",name+"_"+pass);
+                UserService uService=new UserService(LoginActivity.this);
+                boolean flag=uService.login(name, pass);
+
+                if(flag){
+                    Log.i("TAG","登录成功");
+                    Toast.makeText(LoginActivity.this, "登录成功", Toast.LENGTH_LONG).show();
+                    Intent intent = new Intent(LoginActivity.this,RegisterActivity.class);
+                    startActivity(intent);
+                }else{
+                    Log.i("TAG","登录失败");
+                    Toast.makeText(LoginActivity.this, "登录失败", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+        register.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent=new Intent(LoginActivity.this,RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
