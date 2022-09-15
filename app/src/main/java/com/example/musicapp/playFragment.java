3d99d2b2e29d777cd95eba3086327c5329e55567
85008 <850083510@qq.com>
2022-09-15 17:14:59 +0800

    final chu le ge dan he guan li

--- app/src/main/java/com/example/musicapp/playFragment.java
+++ app/src/main/java/com/example/musicapp/playFragment.java
@@ -1,5 +1,6 @@
 package com.example.musicapp;
 
+import android.content.Intent;
 import android.media.MediaScannerConnection;
 import android.os.Bundle;
 
@@ -46,7 +47,7 @@ public class playFragment extends Fragment {
     private Button btn_music_pre;
     private Button btn_music_play;
     private Button btn_music_next;
-    private Button btn_music_list;
+    private Button btn_music_comment;
     private RecyclerView recyclerView;
     private int playmode=0; // 0为列表循环，1为单曲循环，2为随机播放
     private int music_id=1;
@@ -86,7 +87,7 @@ public class playFragment extends Fragment {
         btn_music_pre=view.findViewById(R.id.btn_music_pre);
         btn_music_play=view.findViewById(R.id.btn_music_play);
         btn_music_next=view.findViewById(R.id.btn_music_next);
-        btn_music_list=view.findViewById(R.id.btn_music_list);
+        btn_music_comment=view.findViewById(R.id.btn_music_comment);
         recyclerView =view.findViewById(R.id.recyclerView);
 
 
@@ -194,6 +195,27 @@ public class playFragment extends Fragment {
             }
         });
 
+        // 评论
+        btn_music_comment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                String musicname = music_name.getText().toString();
+//                String singer = music_singer.getText().toString();
+                if(music_name.getText().toString().equals("歌曲名") || music_name.getText().toString().isEmpty())
+                {
+                    Toast.makeText(getContext(),"请先选择歌曲！",Toast.LENGTH_SHORT).show(); // 提示
+                }
+                else // 已选择歌曲
+                {
+                    Toast.makeText(getContext(),music_name.getText().toString(),Toast.LENGTH_SHORT).show(); // 提示
+                    Intent intent=new Intent(getContext(), CommentActivity.class);
+                    intent.putExtra("music_name",music_name.getText().toString());
+                    intent.putExtra("music_singer",music_singer.getText().toString());
+                    startActivity(intent);
+                }
+
+            }
+        });
 
 
     }
